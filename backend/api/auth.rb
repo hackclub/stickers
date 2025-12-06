56981c32abf275ba6d43feb5c6073e60f695c03f
24c02 <163450896+24c02@users.noreply.github.com>
2025-12-06 01:44:50 -0500

    abababababababa

--- backend/api/auth.rb
+++ backend/api/auth.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'faraday'
+require 'json'
+
 class Auth < Grape::API
   format :json
   helpers SessionHelpers
@@ -11,11 +14,8 @@ class Auth < Grape::API
 
     get 'oidc/callback' do
       auth = env['omniauth.auth']
-      session[:user] = {
-        id: auth.uid,
-        email: auth.info.email,
-        name: auth.info.name
-      }
+      user = User.find_or_create_from_oidc(auth)
+      session[:user_id] = user.id
       redirect ENV.fetch('AUTH_SUCCESS_REDIRECT', '/')
     end
 
@@ -27,6 +27,46 @@ class Auth < Grape::API
     get :me do
       error!('Unauthorized', 401) unless current_user
       current_user
+    end
+
+    get 'hackatime/callback' do
+      error!('Please log in first', 401) unless current_user
+
+      auth = env['omniauth.auth']
+      access_token = auth&.credentials&.token.to_s
+
+      uid = nil
+      if access_token.present?
+        begin
+          conn = Faraday.new(url: 'https://hackatime.hackclub.com')
+          response = conn.get('/api/v1/authenticated/me') do |req|
+            req.headers['Authorization'] = "Bearer #{access_token}"
+            req.headers['Accept'] = 'application/json'
+          end
+          if response.success?
+            body = JSON.parse(response.body) rescue {}
+            uid = body['id'].to_s
+          end
+        rescue Faraday::Error => e
+          error!("Hackatime API error: #{e.message}", 502)
+        rescue StandardError => e
+          error!("Unexpected error: #{e.message}", 500)
+        end
+      end
+
+      error!('Could not determine Hackatime user. Try again.', 400) if uid.blank?
+
+      session[:hackatime] = {
+        uid: uid,
+        access_token: access_token
+      }
+
+      redirect ENV.fetch('HACKATIME_SUCCESS_REDIRECT', '/')
+    end
+
+    get :hackatime do
+      error!('Unauthorized', 401) unless current_user
+      session[:hackatime] || {}
     end
   end
 end
--- backend/api/session_helpers.rb
+++ backend/api/session_helpers.rb
@@ -6,7 +6,7 @@ module SessionHelpers
   end
 
   def current_user
-    session[:user]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def authenticate!
